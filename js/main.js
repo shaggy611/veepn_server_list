3a4586856da85c8f0a8f2fc6f6e2a92bb54544d2
shaggy611 <sevczik11@gmail.com>
2021-02-15 03:59:11 +0200

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 window.onload = function () {
+    let collapseButton = document.querySelector(".js-click-to-collapse");
 
+    collapseButton.onclick = function () {
+        let collapsElemClassList = document.querySelector(".js-collapse").classList;
+        if (collapsElemClassList == "_bg-light-blue-bradius js-collapse") {
+            collapsElemClassList.add("_collapsList");
+        }
+        else {
+            collapsElemClassList.remove("_collapsList");
+        }
+    }
 };
