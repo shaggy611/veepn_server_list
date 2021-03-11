97ba5fc0648f9104cfe16d0c790d032e52d855d1
shaggy611 <sevczik11@gmail.com>
2021-03-11 15:47:15 +0200

    done

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ window.onload = function () {
     let collapseBtn = document.querySelector(".js-click-to-collapse");
     collapseButton.onclick = function () {
         document.querySelector(".js-collapse").classList.toggle("_collapsList");
+        document.querySelector(".locations__collapse-btn-wrapper").classList.toggle("_disable-box-shadow");
         collapseBtn.classList.toggle("_pointer-down");
         collapseBtn.textContent = collapseBtn.textContent === "Show more countries" ? "Show less countries" : "Show more countries";
     }
